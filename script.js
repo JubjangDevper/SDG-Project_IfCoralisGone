5a4f1506dde8e066f2869fa66196da1efd208141
DevJubjang <jittiporn9989@gmail.com>
2021-12-13 19:33:35 +0700

    scroll-up

--- script.js
+++ script.js
@@ -2,13 +2,4 @@ document.body.onscroll = function(){
     let scrollY = window.scrollY;
     let viewHeight = window.innerHeight;
     document.querySelector("#yyy").innerText = scrollY;
-
-    graybg = document.querySelector('#img-l11-1').offsetTop;
-    if(scrollY+viewHeight-200 < graybg){
-        document.querySelector('#bg').dataset.active = 1;
-    }else{
-        document.querySelector('#bg').dataset.active = 0;
-    }
-
-    
 }
